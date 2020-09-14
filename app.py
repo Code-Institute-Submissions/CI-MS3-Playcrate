ca9ac84b4ead2dd21216320d21b322211fdc8fe1
Kieran Murray <kohyr@rocketmail.com>
2020-09-14 18:15:30 +0100

    Add GameData class  & Update_Game_Data Function

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField , SelectMultipleField, TextAreaField
+from wtforms import StringField, SelectMultipleField, TextAreaField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, URL
 import os
@@ -9,12 +9,28 @@ import datetime
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 
+class GameData:
+    def __init__(self, title, release_date, developer, publisher, genre, game_description, trailer, wikipedia, front_cover, back_cover):
+        self.title = title
+        self.release_date = release_date
+        self.developer = developer
+        self.publisher = publisher
+        self.genre = genre
+        self.game_description = game_description
+        self.trailer = trailer
+        self.wikipedia = wikipedia
+        self.front_cover = front_cover
+        self.back_cover = back_cover
+
 class GameDataForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     release_date = DateField('Release Date', validators=[DataRequired()])
-    developer = StringField('Developer', validators=[DataRequired()])
-    publisher = StringField('Publisher', validators=[DataRequired()])
-    genre = SelectMultipleField(u'Genre', choices=[('action', 'Action'), ('adventure', 'Adventure'), ('rpg', 'Role Playing Game')])
+    developer = SelectMultipleField(
+        u'Developer', choices=[('core', 'Core'), ('square', 'Square')])
+    publisher = SelectMultipleField(u'Publisher', choices=[(
+        'eidos', 'Eidos'), ('sony', 'Sony Computer Entertainment')])
+    genre = SelectMultipleField(u'Genre', choices=[(
+        'action', 'Action'), ('adventure', 'Adventure'), ('rpg', 'Role Playing Game')])
     game_description = TextAreaField(
         'Game Description', validators=[DataRequired()])
     trailer = StringField('Trailer', validators=[URL()])
@@ -22,15 +38,23 @@ class GameDataForm(FlaskForm):
     front_cover = StringField('Front Cover', validators=[URL()])
     back_cover = StringField('Back Cover', validators=[URL()])
 
+
 @app.route("/")
 def home():
     return render_template('home.html', games=db.games.find())
+
 
 @app.route("/add-game/")
 def add_game():
     form = GameDataForm()
     return render_template('add-game.html', form=form)
 
+
+@app.route("/update-game-data/", methods=('GET', 'POST'))
+def update_game_data():
+    form = GameDataForm()
+    if form.validate_on_submit():
+        return redirect("/")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
