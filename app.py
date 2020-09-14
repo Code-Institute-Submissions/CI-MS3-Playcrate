cef70b3bf63b4a53029046e40dccc79bc403e820
Kieran Murray <kohyr@rocketmail.com>
2020-09-14 10:21:16 +0100

    Import flask_wtf +  datetime. Create Add-Game page

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-import os, db
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired, URL
+import os
+import db
+import datetime
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template('home.html', games = db.games.find())
+    return render_template('home.html', games=db.games.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
