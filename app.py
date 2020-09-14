d0f85c2d26ff33613dfeb3acd95a5c06e3ed2113
Kieran Murray <kohyr@rocketmail.com>
2020-09-14 10:33:45 +0100

    Add GameDataForm

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField , SelectMultipleField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, URL
 import os
@@ -8,6 +8,18 @@ import db
 import datetime
 app = Flask(__name__)
 
+class GameDataForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    release_date = DateField('Release Date', validators=[DataRequired()])
+    developer = StringField('Developer', validators=[DataRequired()])
+    publisher = StringField('Publisher', validators=[DataRequired()])
+    genre = SelectMultipleField(u'Genre', choices=[('action', 'Action'), ('adventure', 'Adventure'), ('rpg', 'Role Playing Game')])
+    game_description = StringField(
+        'Game Description', validators=[DataRequired()])
+    trailer = StringField('Trailer', validators=[URL()])
+    wikipedia = StringField('Wikipedia', validators=[URL()])
+    front_cover = StringField('Front Cover', validators=[URL()])
+    back_cover = StringField('Back Cover', validators=[URL()])
 
 @app.route("/")
 def home():
