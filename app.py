b8d21daea8a6e33142c55bffa6d1462fffd0314e
Kieran Murray <kohyr@rocketmail.com>
2020-09-16 15:35:34 +0100

    Add Update & Delete Game Data Functions

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, request
+from bson import ObjectId
+from collections import namedtuple
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectMultipleField, TextAreaField
+from wtforms import StringField, SelectMultipleField, TextAreaField, BooleanField, IntegerField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, URL
 import os
@@ -11,7 +13,8 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 
 class GameData:
-    def __init__(self, title, release_date, developer, publisher, genre, game_description, trailer, wikipedia, front_cover, back_cover):
+    def __init__(self, doc_id, title, release_date, developer, publisher, genre, game_description, trailer, wikipedia, front_cover, back_cover,data_exists_in_db):
+        self.doc_id = doc_id
         self.title = title
         self.release_date = release_date
         self.developer = developer
@@ -22,9 +25,11 @@ class GameData:
         self.wikipedia = wikipedia
         self.front_cover = front_cover
         self.back_cover = back_cover
+        self.data_exists_in_db = data_exists_in_db
 
 
 class GameDataForm(FlaskForm):
+    doc_id = StringField()
     title = StringField('Title', validators=[DataRequired()])
     release_date = DateField('Release Date', validators=[DataRequired()])
     developer = SelectMultipleField(
@@ -37,14 +42,15 @@ class GameDataForm(FlaskForm):
     wikipedia = StringField('Wikipedia', validators=[URL()])
     front_cover = StringField('Front Cover', validators=[URL()])
     back_cover = StringField('Back Cover', validators=[URL()])
+    data_exists_in_db = BooleanField('Existing Data', default=False)
 
 
-@app.route("/")
+@ app.route("/")
 def home():
     return render_template('home.html', games=db.games.find())
 
 
-@app.route("/add-game/")
+@ app.route("/add-game/")
 def add_game():
     form = GameDataForm()
     if request.method == 'GET':
@@ -52,13 +58,11 @@ def add_game():
         update_form_choices(form)
     return render_template('add-game.html', form=form)
 
-
-@app.route("/add-game-data/", methods=('GET', 'POST'))
+@ app.route("/add-game-data/", methods=('GET', 'POST'))
 def add_game_data():
     form = GameDataForm()
 
     if request.method == 'POST':
-
         # Check if the game title already exists in the DB, if it already exists it can not be added to the DB,
         # return to the form
 
@@ -104,8 +108,8 @@ def add_game_data():
             genre_doc = {'name': new_genre}
             db.genres.insert_one(genre_doc)
         form.genre.choices.append(new_genres)
-
         if form.validate:
+            doc_id = form.doc_id.data
             title = form.title.data
             release_date = datetime.datetime.combine(
                 form.release_date.data, datetime.time.min)
@@ -127,14 +131,22 @@ def add_game_data():
                          'trailer': trailer,
                          'wikipedia': wikipedia,
                          'front_cover': front_cover,
-                         'back_cover': back_cover}
-
-            db.games.insert_one(game_data)
+                         'back_cover': back_cover, }
+            # Check if the submitted form is data edited from an existing game in the DB if it is then update the
+            # data for that game if not then add the new game to the DB
+            if form.data_exists_in_db.data == True:
+                    print("Updating Existing Data")
+                    query = {"_id" : ObjectId(doc_id) }
+                    db.games.replace_one(query, game_data)
+            else:
+                print("Adding New Data")
+                db.games.insert_one(game_data)
+            
             return redirect("/")
     return redirect("/")
 
 
-@app.route('/games/<game_name>')
+@ app.route('/games/<game_name>')
 def view_game(game_name):
     game_to_view = {}
     for game in db.games.find():
@@ -144,30 +156,35 @@ def view_game(game_name):
     return render_template('view-game.html', game=game_to_view)
 
 
-@app.route('/edit-game/<game_title>')
+@ app.route('/edit-game/<game_title>')
 def edit_game(game_title):
-    form_data = {}
+    game_data_in_db = {}
     for game in db.games.find():
         if game["title"] == game_title:
-            form_data = game
-
+            game_data_in_db = game
     game_data = GameData(
-        title=form_data['title'],
-        release_date=form_data['release_date'],
-        developer=form_data['developer'],
-        publisher=form_data['publisher'],
-        genre=form_data['genre'],
-        game_description=form_data['game_description'],
-        trailer=form_data['trailer'],
-        wikipedia=form_data['wikipedia'],
-        front_cover=form_data['front_cover'],
-        back_cover=form_data['back_cover'],
+        doc_id=game_data_in_db['_id'],
+        title=game_data_in_db['title'],
+        release_date=game_data_in_db['release_date'],
+        developer=game_data_in_db['developer'],
+        publisher=game_data_in_db['publisher'],
+        genre = game_data_in_db['genre'],
+        game_description=game_data_in_db['game_description'],
+        trailer=game_data_in_db['trailer'],
+        wikipedia=game_data_in_db['wikipedia'],
+        front_cover=game_data_in_db['front_cover'],
+        back_cover=game_data_in_db['back_cover'],
+        data_exists_in_db = True
     )
-
     form = GameDataForm(obj=game_data)
     update_form_choices(form)
     return render_template("add-game.html", form=form)
 
+
+@app.route('/delete/<game_id>')
+def delete_game(game_id):
+    db.games.remove({'_id': ObjectId(game_id)})
+    return redirect('/')
 
 def update_form_choices(form):
         for developer in db.developers.find():
@@ -178,7 +195,7 @@ def update_form_choices(form):
                 (publisher['name'], publisher['name']))
         for genre in db.genres.find():
             form.genre.choices.append((genre['name'], genre['name']))
-    
+
 @app.route("/update-game-data/", methods=('GET', 'POST'))
 def update_game_data():
     form = GameDataForm()
